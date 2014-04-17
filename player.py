94e8e3db89943a0d457da995b373160831814272
JoshuaGilstrap <jgilstrapper@gmail.com>
2014-04-17 12:45:52 -0400

    Update player.py

--- player.py
+++ player.py
@@ -6,7 +6,14 @@ class player():
     def __init__(self):
         self.value = 0
         self.score = label(pos=(-350,350,0), text='Score: %d' % self.value)
+        self.incremented = False
 
     def incrementScore(self):
         self.value = self.value + 1
         self.score = label(pos=(-350,350,0), text='Score: %d' % self.value)
+
+
+#include player in obstacle
+#add 'p1.incremented = False to obstacle movement if statement
+#create new if statement in obstacle with same condition but past 0 and incremented False
+#inside, increment score and set incremented to True
