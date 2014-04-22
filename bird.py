81f52424eb0642def086f82c26d1eca66af216bd
liaoj12 <jackliao2012@gmail.com>
2014-04-22 14:19:34 -0400

    Update bird.py

--- bird.py
+++ bird.py
@@ -2,8 +2,9 @@ from __future__ import division
 from visual import*
 #from obstacle import*
 import math
+#import Image
 
-class bird(sphere):
+class bird():
     def __init__(self):
         self.ball = sphere(pos=(0,0,0),radius=30, color=color.red)
         self.ball.velocity = vector(0,0,0)
@@ -18,7 +19,7 @@ class bird(sphere):
     def moveBird(self, dt):
         self.ball.velocity.y = self.ball.velocity.y - 9.8*dt
         self.ball.pos = self.ball.pos + (0,self.ball.velocity.y*dt,0)
-        if self.ball.pos.y < -375:
+        if self.ball.pos.y < -450:
             self.bottom = True
             print 'hit bottom'
         
