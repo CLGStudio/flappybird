f22be5773516e958fd661a30f27e8ffc8038876c
liaoj12 <jackliao2012@gmail.com>
2014-04-22 14:19:49 -0400

    Update game.py

--- game.py
+++ game.py
@@ -6,20 +6,24 @@ from player import*
 import math
 
 #set the screen window size
-scene.range = 500
-scene.width = 500
+sceneRange = 500
+sceneWidth = 500
+scene.range = sceneRange
+scene.width = sceneWidth
 scene.autoscale = 0
+scene.title = 'Flappy Bird!'
 
 #create ball
 b1=bird()
 
 #create pipe obstacles
-obs1=obstacle(500,500)
+obs1=obstacle(sceneRange)
+obs2=obstacle(sceneRange*2)
 #obs2=cylinder(pos=(150,-150,0),axis=(0,150,0),radius=75/2,color=color.green)
 
 #create obstacle velocity constants
-dt=.5
-Vx=15
+dt=.01
+Vx=25
 
 #create player and draw score
 p1=player()
@@ -29,15 +33,26 @@ def keyInput(keyIn):
     if obs1.detection(b1) == True:
         if (keyIn.key == 'up' or keyIn.key == 'space'):#if UP and the ball is not at the top of the screen, can move
             b1.jump()
-            print 'jumped'
+            #print 'jumped'
 #}
         
 scene.bind('keydown', keyInput)#when the user presses a key, go to key handler function
 
 #while no collision has been detected{
-while obs1.detection(b1) == True and b1.bottom == False:
-    sleep(.0001)
-    b1.moveBird(dt)
-    obs1.moveObstacle(dt,Vx,500)
-    print 'r'
+#while obs1.detection(b1) == True and b1.bottom == False:
+#   b1.moveBird(dt)
+#   obs1.moveObstacle(dt,Vx,500)
+#    rate(1000)
+    #print 'r'
 #}
+#def moveObs(obstacles,bird,dt,Vx):
+while obs1.detection(b1) == True and obs2.detection(b1) == True and b1.bottom == False:
+    b1.moveBird(dt)
+    obs1.moveObstacle(dt,Vx,500,p1)
+    obs2.moveObstacle(dt,Vx,500,p1)
+    rate(1000)
+
+#moveObs(obs1,b1,dt,Vx)
+#moveObs(obs2,b1,dt,Vx)
+
+
