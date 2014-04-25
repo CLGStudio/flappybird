4257931a72880ad4c3dad6af8060a66f1dc4c162
liaoj12 <jackliao2012@gmail.com>
2014-04-25 13:54:31 -0400

    Update bird.py

--- bird.py
+++ bird.py
@@ -4,12 +4,14 @@ from visual import*
 import math
 #import Image
 
+gravity = 9.8
+
 class bird():
     def __init__(self):
         self.ball = sphere(pos=(0,0,0),radius=30, color=color.red)
         self.ball.velocity = vector(0,0,0)
         self.bottom = False
-        self.jumpSteps = 50
+        self.jumpSteps = 70
         #self.maxVy = 100
 
         #self.pos = (0,0,0)
@@ -17,7 +19,7 @@ class bird():
         #self.color = color.red
 
     def moveBird(self, dt):
-        self.ball.velocity.y = self.ball.velocity.y - 9.8*dt
+        self.ball.velocity.y = self.ball.velocity.y - 2*gravity*dt
         self.ball.pos = self.ball.pos + (0,self.ball.velocity.y*dt,0)
         if self.ball.pos.y < -450:
             self.bottom = True
