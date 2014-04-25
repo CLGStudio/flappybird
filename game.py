b09d392fbc8a6b2d1f6520ac001bb2bf545ede4c
liaoj12 <jackliao2012@gmail.com>
2014-04-25 16:07:36 -0400

    Update game.py

--- game.py
+++ game.py
@@ -13,6 +13,40 @@ scene.width = sceneWidth
 scene.autoscale = 0
 scene.title = 'Flappy Bird!'
 
+#handle when user presses arrow key{
+def keyInput(keyIn):
+    if obs1.detection(b1) == True:
+        if (keyIn.key == 'up' or keyIn.key == 'space'):#if UP and the ball is not at the top of the screen, can move
+            b1.jump()
+            #print 'jumped'
+        if keyIn.key == '\n':
+            startLabel.visible = False
+            StartGame()
+            if p1.value>temp:
+                highscore=p1.value
+                highscore=str(highscore)
+                fileO.write(highscore)
+            else:
+                highscore=temp
+                highscore=str(temp)
+                fileO.write(highscore)
+            fileA.close()
+            fileI.close()
+            fileO.close()
+#}
+
+        
+scene.bind('keydown', keyInput)#when the user presses a key, go to key handler function
+
+
+def StartGame():
+    while obs1.detection(b1) == True and obs2.detection(b1) == True and b1.bottom == False:
+        b1.moveBird(dt)
+        obs1.moveObstacle(dt,Vx,500,p1)
+        obs2.moveObstacle(dt,Vx,500,p1)
+        rate(1000)
+
+
 #open file to load highscores and prepare to write
 fileA = open("highscore.txt","a+")
 fileI = open("highscore.txt","r")
@@ -50,39 +84,6 @@ Vx=25
 p1=player()
 
 startLabel = label(pos=(0,0,0),text='Press Enter to start!')
-
-#handle when user presses arrow key{
-def keyInput(keyIn):
-    if obs1.detection(b1) == True:
-        if (keyIn.key == 'up' or keyIn.key == 'space'):#if UP and the ball is not at the top of the screen, can move
-            b1.jump()
-            #print 'jumped'
-        if keyIn.key == '\n':
-            startLabel.visible = False
-            StartGame()
-            if p1.value>temp:
-                highscore=p1.value
-                highscore=str(highscore)
-                fileO.write(highscore)
-            else:
-                highscore=temp
-                highscore=str(temp)
-                fileO.write(highscore)
-            fileA.close()
-            fileI.close()
-            fileO.close()
-#}
-
-        
-scene.bind('keydown', keyInput)#when the user presses a key, go to key handler function
-
-
-def StartGame():
-    while obs1.detection(b1) == True and obs2.detection(b1) == True and b1.bottom == False:
-        b1.moveBird(dt)
-        obs1.moveObstacle(dt,Vx,500,p1)
-        obs2.moveObstacle(dt,Vx,500,p1)
-        rate(1000)
 
 #moveObs(obs1,b1,dt,Vx)
 #moveObs(obs2,b1,dt,Vx)
