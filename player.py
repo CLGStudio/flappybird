350bd582c2b75c8fb38b8d67525dfbe4e6b8ed31
JoshuaGilstrap <jgilstrapper@gmail.com>
2014-04-28 19:01:23 -0400

    added comments

--- player.py
+++ player.py
@@ -2,19 +2,21 @@ from __future__ import division
 from visual import*
 import math
 
+#create the player class
 class player():
+    #define the initialization method
     def __init__(self):
+        #set the score (value) to zero, create the label which will display the score
+        #and initialize the incremented flag to False (tracks when to increment score)
         self.value = 0
         self.score = label(pos=(-350,350,0), text='Score: %d' % self.value)
         self.incremented = False
 
+    #define the increment score method
+    #the 'incremented' flag is set from obstacle.py when the bird passes through a set of obstacles
+    #(i.e. when the obstacles pass the middle of the scene)
     def incrementScore(self):
+        #increment the score and update the display
         self.value = self.value + 1
         self.score = label(pos=(-350,350,0), text='Score: %d' % self.value)
 
-
-
-#include player in obstacle
-#add 'p1.incremented = False to obstacle movement if statement
-#create new if statement in obstacle with same condition but past 0 and incremented False
-#inside, increment score and set incremented to True
