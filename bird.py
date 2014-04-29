bc471f46b5c705e8752553f5d7e0e03ddf5ff160
liaoj12 <jackliao2012@gmail.com>
2014-04-29 13:37:57 -0400

    Update bird.py

--- bird.py
+++ bird.py
@@ -4,13 +4,19 @@ from visual import*
 import math
 #import Image
 
+#creating gravity constant
 gravity = 9.8
+difficulty = 2
+screenHeight = 500
 
 class bird():
     def __init__(self):
+        #creating ball and setting default values
         self.ball = sphere(pos=(0,0,0),radius=30, color=color.red)
+        #setting default velocity
         self.ball.velocity = vector(0,0,0)
         self.bottom = False
+        #how far the ball jumps
         self.jumpSteps = 70
         #self.maxVy = 100
 
@@ -18,15 +24,22 @@ class bird():
         #self.radius = 30
         #self.color = color.red
 
+    #function to move bird at a constant downward velocity
     def moveBird(self, dt):
-        self.ball.velocity.y = self.ball.velocity.y - 2*gravity*dt
+        #updating velocity of ball
+        #the 2 is here to improve gaming experience/to make it a bit more difficult
+        self.ball.velocity.y = self.ball.velocity.y - difficulty*gravity*dt
+        #updating the ball's position with new velocity
         self.ball.pos = self.ball.pos + (0,self.ball.velocity.y*dt,0)
-        if self.ball.pos.y < -450:
+        #condition statement to check if ball hits the ground
+        if self.ball.pos.y < -(screenHeight-50):
             self.bottom = True
             print 'hit bottom'
-        
+    #function to "jump"    
     def jump(self):
-        if self.ball.pos.y > 500:
-            self.ball.pos.y = 500
+        #condition statement to check if ball is above the screen height. If it is set position to screen height.
+        if self.ball.pos.y > screenHeight:
+            self.ball.pos.y = screenHeight
+        #set velocity = to the jumpsteps var.
         else:
             self.ball.velocity.y = self.jumpSteps
